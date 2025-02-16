80e274d453c799bb80b4aae3231db75b0e31fca8
Cory Sturgis <casturgi@gmail.com>
2025-02-16 14:15:06 -0800

    update | remove sanity api key from required build vars

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -25,8 +25,7 @@ console.log('Current environment:', {
 // Validate required environment variables
 const requiredEnvs = [
   'NEXT_PUBLIC_SANITY_PROJECT_ID',
-  'NEXT_PUBLIC_SANITY_DATASET',
-  'SANITY_API_TOKEN'
+  'NEXT_PUBLIC_SANITY_DATASET' 
 ]
 
 requiredEnvs.forEach(env => {
